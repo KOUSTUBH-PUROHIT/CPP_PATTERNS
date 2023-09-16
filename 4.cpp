7361a8e74010f38a74bf4b0f2bbb3809b3ee16b0
KOUSTUBH-PUROHIT <122957537+KOUSTUBH-PUROHIT@users.noreply.github.com>
2023-09-16 23:26:11 +0530

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,30 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+
+    int i = 1;
+    int n;
+    cin >>  n;
+
+    while(i<=n){
+        int j = 1;
+        while(j<=i){
+        cout<< "*"<< " ";
+        j = j + 1;
+          }
+        cout<<endl;
+        i = i+1;
+
+    }
+
+    return 0;
+}
+
+output =
+5
+*
+* *
+* * *
+* * * *
+* * * * *
